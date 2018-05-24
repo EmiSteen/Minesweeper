004f9ef9a52be46aeccd29b8802f5e89cfee6601
EmiSteen <emile@steenkamps.org>
2018-05-24 17:09:34 +0200

    remove hello world

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("Hello World!");
         try {
-            gcghfdgfdgfdgf
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
         } catch (Exception e) {
             //do nothing
